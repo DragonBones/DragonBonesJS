edc130bfcf9b326a2f7a32dad6975f5436fc6cf4
akdcl <akdcl@sina.com>
2017-07-26 18:37:45 +0800

    update egret

--- Egret/Demos/src/Main.ts
+++ Egret/Demos/src/Main.ts
@@ -2,8 +2,8 @@ class Main extends egret.DisplayObjectContainer {
     public constructor() {
         super();
 
-        // this.addChild(new HelloDragonBones());
-        this.addChild(new ReplaceSlotDisplay());
+        this.addChild(new HelloDragonBones());
+        // this.addChild(new ReplaceSlotDisplay());
         // this.addChild(new coreElement.Game());
 
         // this.addChild(new PerformanceTest());
