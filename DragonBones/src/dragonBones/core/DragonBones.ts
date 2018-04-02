7ca6e6e324624a3477be644653102b6844002e92
duanchunlei <duanchunlei@egret.com>
2018-04-02 12:17:58 +0800

    modify clock update

--- DragonBones/src/dragonBones/core/DragonBones.ts
+++ DragonBones/src/dragonBones/core/DragonBones.ts
@@ -299,6 +299,8 @@ namespace dragonBones {
                 this._objects.length = 0;
             }
 
+            this._clock.advanceTime(passedTime);
+
             if (this._events.length > 0) {
                 for (let i = 0; i < this._events.length; ++i) {
                     const eventObject = this._events[i];
@@ -315,8 +317,6 @@ namespace dragonBones {
 
                 this._events.length = 0;
             }
-
-            this._clock.advanceTime(passedTime);
         }
 
         public bufferEvent(value: EventObject): void {
