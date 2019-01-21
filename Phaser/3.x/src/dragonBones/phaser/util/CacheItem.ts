65fdc1fcb4663ff652852aa31462f06c99d1afad
J.C <jcyuan.space@gmail.com>
2019-01-21 22:18:40 +0800

    CacheItem.ts actually is unnecessary, only db data should be cached so remove this, more details please refer to the changes of DragonBonesFile.ts

--- Phaser/3.x/src/dragonBones/phaser/util/CacheItem.ts
+++
@@ -1,9 +0,0 @@
-namespace dragonBones.phaser.util {
-    export class CacheItem {
-        boneData:DragonBonesData;
-        scale:number;
-        textureData:any;
-        textureImage:any;
-        textureImageKey:string;
-    }
-}
