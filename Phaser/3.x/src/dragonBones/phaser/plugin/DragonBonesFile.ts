4b6caa4d89aa7e1d208bc0506c454ba9f5a7dc9c
J.C <jcyuan.space@gmail.com>
2019-01-21 22:27:36 +0800

    1, remove global plugin (db instance should be a part of scene, for example you need to pause your current scene) 2, add custom cache store for db bone data 3, use a manager to get file loaders, so users could replace the default loaders with their own ones.

--- Phaser/3.x/src/dragonBones/phaser/plugin/DragonBonesFile.ts
+++ Phaser/3.x/src/dragonBones/phaser/plugin/DragonBonesFile.ts
@@ -1,7 +1,5 @@
 namespace dragonBones.phaser.plugin {
     export class DragonBonesFile extends Phaser.Loader.MultiFile {
-        private _scale: number;
-
         constructor(loader: Phaser.Loader.LoaderPlugin,
                     key: string | object,
                     textureURL?: string,
@@ -9,13 +7,16 @@ namespace dragonBones.phaser.plugin {
                     boneURL?: string,
                     textureXhrSettings?: XHRSettingsObject,
                     atlasXhrSettings?: XHRSettingsObject,
-                    boneXhrSettings?: XHRSettingsObject,
-                    scale: number = 1.0) {
-            let image;
-            let data;
-            let boneData;
+                    boneXhrSettings?: XHRSettingsObject) {
+            let image: Phaser.Loader.FileTypes.ImageFile;
+            let data: Phaser.Loader.FileTypes.JSONFile;
+            let boneData: Phaser.Loader.File;
 
             let keyName: string;
+
+            const binFileType = FileTypes.getType(FileTypes.BINARY);
+            const jsonFileType = FileTypes.getType(FileTypes.JSON);
+            const imageFileType = FileTypes.getType(FileTypes.IMAGE);
 
             const createBoneFileByType = (loader: Phaser.Loader.LoaderPlugin, key: string, boneURL: string, boneXhrSettings?: XHRSettingsObject): Phaser.Loader.File => {
                 let type = "json";
@@ -33,8 +34,8 @@ namespace dragonBones.phaser.plugin {
                 }
 
                 return type === "bin" ?
-                    new Phaser.Loader.FileTypes.BinaryFile(loader, key, boneURL, boneXhrSettings) :
-                    new Phaser.Loader.FileTypes.JSONFile(loader, key, boneURL, boneXhrSettings);
+                    new binFileType(loader, key, boneURL, boneXhrSettings) :
+                    new jsonFileType(loader, key, boneURL, boneXhrSettings);
             };
 
             if (Phaser.Utils.Objects.IsPlainObject(key as object)) {
@@ -42,14 +43,14 @@ namespace dragonBones.phaser.plugin {
                 const config = key as object;
                 keyName = Phaser.Utils.Objects.GetFastValue(config, 'key');
 
-                image = new Phaser.Loader.FileTypes.ImageFile(loader, {
+                image = new imageFileType(loader, {
                     key: keyName,
                     url: Phaser.Utils.Objects.GetFastValue(config, 'textureURL'),
                     extension: Phaser.Utils.Objects.GetFastValue(config, 'textureExtension', 'png'),
                     xhrSettings: Phaser.Utils.Objects.GetFastValue(config, 'textureXhrSettings')
                 });
 
-                data = new Phaser.Loader.FileTypes.JSONFile(loader, {
+                data = new jsonFileType(loader, {
                     key: keyName + "_atlasjson",
                     url: Phaser.Utils.Objects.GetFastValue(config, 'atlasURL'),
                     extension: Phaser.Utils.Objects.GetFastValue(config, 'atlasExtension', 'json'),
@@ -63,36 +64,25 @@ namespace dragonBones.phaser.plugin {
             } else {
                 keyName = key as string;
 
-                image = new Phaser.Loader.FileTypes.ImageFile(loader, keyName, textureURL, textureXhrSettings);
-                data = new Phaser.Loader.FileTypes.JSONFile(loader, keyName + "_atlasjson", atlasURL, atlasXhrSettings);
+                image = new imageFileType(loader, keyName, textureURL, textureXhrSettings);
+                data = new jsonFileType(loader, keyName + "_atlasjson", atlasURL, atlasXhrSettings);
                 boneData = createBoneFileByType(loader, keyName, boneURL, boneXhrSettings);
             }
 
-            super(loader, 'dragonbone', keyName, [ image, data, boneData ]);
+            boneData.cache = loader["cacheManager"].custom.dragonbone;
 
-            this._scale = scale;
+            super(loader, 'dragonbone', keyName, [ image, data, boneData ]);
         }
 
         addToCache(): void {
             if (this.isReadyToProcess()) {
-                const binaryParser = new BinaryDataParser();
-
                 const image = this.files[0];
                 const json = this.files[1];
                 const bone = this.files[2];
 
-                const cacheItem:Phaser.Cache.BaseCache = this.loader["cacheManager"].addCustom("dragonbones");
-
-                const dragonBonesData = binaryParser.parseDragonBonesData(bone.data, this._scale);
-
-                const cacheObject:util.CacheItem = {
-                    boneData: dragonBonesData,
-                    scale: this._scale,
-                    textureData: json.data,
-                    textureImage: image.data,
-                    textureImageKey: image.key
-                }
-                cacheItem.add(bone.key, cacheObject);
+                json.addToCache();
+                bone.addToCache();
+                this.loader.textureManager.addImage(image.key, image.data);
 
                 this.complete = true;
             }
--- Phaser/3.x/src/dragonBones/phaser/plugin/DragonBonesPlugin.ts
+++ Phaser/3.x/src/dragonBones/phaser/plugin/DragonBonesPlugin.ts
@@ -1,67 +1,39 @@
 namespace dragonBones.phaser.plugin {
-    export class DragonBonesPlugin extends Phaser.Plugins.BasePlugin {
-        private _dbInst: DragonBones;
-
-        constructor(pluginManager: Phaser.Plugins.PluginManager) {
-            super(pluginManager);
-
-            this._dbInst = new DragonBones(new util.EventDispatcher());
-
-            const renderer = this.game.renderer as Phaser.Renderer.WebGL.WebGLRenderer;
-            renderer.addPipeline('PhaserTextureTintPipeline', new pipeline.TextureTintPipeline({ game: this.game, renderer }));
-
-            pluginManager.installScenePlugin("DragonBonesScenePlugin", DragonBonesScenePlugin, "dragonbone");
-        }
-
-        start(): void {
-            super.start();
-            this.game.events.on('step', this.updateDBInst, this);
-        }
-
-        stop(): void {
-            super.stop();
-            this.game.events.off('step', this.updateDBInst, this);
-        }
-
-        destroy(): void {
-            this.game.events.off('step', this.updateDBInst, this);
-            super.destroy();
-        }
-
-        private updateDBInst(time: number, delta: number): void {
-            if (!this._dbInst) return;
-            this._dbInst.advanceTime(delta * 0.001);
-        }
-
-        get dbInstance(): DragonBones {
-            return this._dbInst;
-        }
-    }
-
     export class DragonBonesScenePlugin extends Phaser.Plugins.ScenePlugin {
-        private _factory: Factory;
+        protected _dbInst: dragonBones.DragonBones;
+        protected _factory: Factory;
 
         constructor(scene: Phaser.Scene, pluginManager: Phaser.Plugins.PluginManager) {
             super(scene, pluginManager);
+
+            const game = this.game;
+
+            // bone data store
+            game.cache.addCustom("dragonbone");
+
+            if (this.game.config.renderType === Phaser.WEBGL) {
+                const renderer = this.game.renderer as Phaser.Renderer.WebGL.WebGLRenderer;
+                if (!renderer.hasPipeline('PhaserTextureTintPipeline'))
+                    renderer.addPipeline('PhaserTextureTintPipeline', new pipeline.TextureTintPipeline({ game, renderer }));
+            }
+
             pluginManager.registerGameObject("armature", CreateArmatureRegisterHandler);
+            pluginManager.registerFileType("dragonbone", DragonBoneFileRegisterHandler, scene);
         }
 
-        createArmature(armature: string, dragonBones?: string, skinName?: string, atlasTextureName?: string): display.ArmatureDisplay {
-            return this.factory.buildArmatureDisplay(armature, dragonBones, skinName, atlasTextureName);
+        createArmature(armature: string, dragonBones?: string, skinName?: string, atlasTextureName?: string, textureScale = 1.0): display.ArmatureDisplay {
+            const display = this.factory.buildArmatureDisplay(armature, dragonBones, skinName, atlasTextureName, textureScale);
+            this.systems.displayList.add(display);
+            // use db.clock instead, if here we just use this.systems.updateList.add(display), that will cause the db event is dispatched with 1 or more frames delay
+            this._dbInst.clock.add(display.armature);
+
+            return display;
         }
 
         get factory(): Factory {  // lazy instancing
             if (!this._factory) {
-                let dbPlugin: DragonBonesPlugin;
-                for (const p of this.pluginManager.plugins) {
-                    if (p.plugin instanceof DragonBonesPlugin) {
-                        dbPlugin = p.plugin;
-                        break;
-                    }
-                }
-                if (!dbPlugin)
-                    throw new Error("Please add the dragonbone plugin in your GameConfig");
-                this._factory = new Factory(dbPlugin.dbInstance, this.scene);
+                this._dbInst = new dragonBones.DragonBones(new util.EventDispatcher());
+                this._factory = new Factory(this._dbInst, this.scene);
             }
 
             return this._factory;
@@ -74,12 +46,45 @@ namespace dragonBones.phaser.plugin {
         createSlotDisplayPlaceholder(): display.SlotImage | display.SlotSprite {
             return new display.SlotImage(this.scene, 0, 0);
         }
+
+        boot(): void {
+            this.systems.events.once('destroy', this.destroy, this);
+            this.start();
+        }
+
+        start(): void {
+            const ee = this.systems.events;
+
+            ee.on('update', this.update, this);
+            ee.once('shutdown', this.shutdown, this);
+        }
+
+        private update(time: number, delta: number): void {
+            this._dbInst && this._dbInst.advanceTime(delta * 0.001);
+        }
+
+        shutdown(): void {
+            const ee = this.systems.events;
+
+            ee.off('update', this.update, this);
+            ee.off('shutdown', this.shutdown, this);
+        }
+
+        destroy(): void {
+            this.shutdown();
+
+            this._factory =
+            this._dbInst = null;
+
+            this.pluginManager =
+            this.game =
+            this.scene =
+            this.systems = null;
+        }
     }
 
     const CreateArmatureRegisterHandler = function(armature: string, dragonBones?: string, skinName?: string, atlasTextureName?: string): display.ArmatureDisplay {
-        const display = this.scene.dragonbone.createArmature(armature, dragonBones, skinName, atlasTextureName);
-        this.displayList.add(display);
-        return display;
+        return this.scene.dragonbone.createArmature(armature, dragonBones, skinName, atlasTextureName);
     };
 
     const DragonBoneFileRegisterHandler = function(dragonbonesName: string | object,
@@ -88,13 +93,10 @@ namespace dragonBones.phaser.plugin {
             boneURL?: string,
             textureXhrSettings?: XHRSettingsObject,
             atlasXhrSettings?: XHRSettingsObject,
-            boneXhrSettings?: XHRSettingsObject,
-            scale: number = 1.0) {
-        const multifile = new DragonBonesFile(this, dragonbonesName, textureURL, atlasURL, boneURL, textureXhrSettings, atlasXhrSettings, boneXhrSettings, scale);
+            boneXhrSettings?: XHRSettingsObject) {
+        const multifile = new DragonBonesFile(this, dragonbonesName, textureURL, atlasURL, boneURL, textureXhrSettings, atlasXhrSettings, boneXhrSettings);
         this.addFile(multifile.files);
 
         return this;
     };
-
-    Phaser.Loader.FileTypesManager.register("dragonbone", DragonBoneFileRegisterHandler);
 }
