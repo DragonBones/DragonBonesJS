71ccc7d572ecfc6f64eb0be96458b7f99cbf4ac2
William <william.y.kuang@gmail.com>
2019-10-19 12:00:26 +1100

    Files changed from the build script

--- Pixi/Demos/out/DragHelper.js
+++ Pixi/Demos/out/DragHelper.js
@@ -35,8 +35,8 @@ var DragHelper = /** @class */ (function () {
                     this._helpPoint.x = event.data.global.x;
                     this._helpPoint.y = event.data.global.y;
                     armatureDisplay.toLocal(this._helpPoint, this.stage, this._helpPoint);
-                    if (bone.offsetMode !== dragonBones.OffsetMode.Override) {
-                        bone.offsetMode = dragonBones.OffsetMode.Override;
+                    if (bone.offsetMode !== 2 /* Override */) {
+                        bone.offsetMode = 2 /* Override */;
                         bone.offset.x = bone.global.x;
                         bone.offset.y = bone.global.y;
                     }
